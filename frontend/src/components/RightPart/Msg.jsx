3a59486f3895afb6e188bd7d3ee654aaa7be97b3
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2024-12-20 21:06:04 +0530

    chat update

--- frontend/src/components/RightPart/Msg.jsx
+++ frontend/src/components/RightPart/Msg.jsx
@@ -4,12 +4,10 @@ function Msg() {
   return (
     <div>
       <div className="chat chat-end">
-        <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+        <div className="chat-bubble chat-bubble-info">Calm down, Akhilesh.</div>
       </div>
       <div className="chat chat-start">
-        <div className="chat-bubble chat-bubble-warning">
-          To be on the Council at your age.
-        </div>
+        <div className="chat-bubble chat-bubble-warning">how are you?</div>
       </div>
     </div>
   );
