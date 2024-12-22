3cdd4a7866b4ba3b52c5cd708d2800ffcc1e7b73
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2024-12-23 00:54:51 +0530

    jwt added for auth

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,10 +1,13 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import createToken from "../jwt/tokenGenerate.js";
 
 export const signup = async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
   try {
-    if (password !== confirmPassword) console.log("password do not match");
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ error: "User already exists" });
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -14,10 +17,39 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
     await newUser.save();
-
-    res.status(200).json({ message: "User registered successfully" });
+    if (newUser) {
+      createToken(newUser._id, res);
+      res
+        .status(200)
+        .json({ message: "User registered successfully", newUser });
+    }
   } catch (error) {
     console.log("error", error);
     res.status(500).json({ error: "Failed to register user" });
+  }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    createToken(user._id, res);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to login" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt-token");
+    res.send({ message: "User logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to logout" });
   }
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { signup } from "../controller/user.controller.js";
+import { login, logout, signup } from "../controller/user.controller.js";
 
 router.post("/signup", signup);
+
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
