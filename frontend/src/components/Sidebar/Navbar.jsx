2d8ee4291d43e2ed9a7ba9a40eb6bb00d35ffa8e
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-02-07 21:43:22 +0530

    new app name

--- frontend/src/components/Sidebar/Navbar.jsx
+++ frontend/src/components/Sidebar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="bg-black border border-black h-[45px] text-white flex items-center">
       <FaWhatsapp className="ml-[19px] mr-2 text-green-500 text-2xl" />
-      <h1 className="ml-2">WhatsApp</h1>
+      <h1 className="ml-2"> Meats App</h1>
     </div>
   );
 }
